5adbf3d81c08e8d09590819024dd0f444cddfa4d
Nansubuga Marie <nansubugamarie@gmail.com>
2026-04-23 22:43:33 +0300

    task 5 complete

--- secure.js
+++ secure.js
@@ -1,8 +1,25 @@
 const { DatabaseSync } = require('node:sqlite');
 const db = new DatabaseSync('nyondo_stock.db');
 
-// safe search
-function searchProductSafe(name) {
+// Validation 
+function isValidName(name){
+      return typeof name === 'string' && name.length && !/[<>;]/.test(name);
+}
+
+function isValidUsername(username){
+      return typeof username === 'string' && username.length > 0 && !username.includes('');
+}
+
+function isValidPassword(password){
+      return typeof password === 'string' && password.length >=6;
+}
+// safe search with validation
+function searchProductSafe(name){
+      if(!isValidName(name)){
+            console.log('Error: Invalid Product name');
+            return null;
+      }
+
  const query = `SELECT id, name, cost_price, sell_price, stock FROM products WHERE name LIKE ?`;
  const rows =db.prepare(query).all(`%${name}%`);
  console.log('Result:', rows);
@@ -10,8 +27,18 @@ function searchProductSafe(name) {
 }
 
 
-// safe login
+// safe login with validation
 function loginSafe(username,password){
+      if(!isValidUsername(username)){
+      console.log('Error: Invalid Username');
+      return null;
+      }
+
+if (!isValidPassword(password)){
+      console.log('Error: Invalid password');
+      return null;
+}
+
       const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
       const row = db.prepare(query).get(username,password);
 
@@ -20,11 +47,38 @@ function loginSafe(username,password){
 
 }
 
-// testing
-console.log('Test 1:', searchProductSafe("'OR 1=1--"));
+// test cases for ptoduct search
+console.log('Test 1:', searchProductSafe('cement'));
 
-console.log('Test 2:', searchProductSafe("'UNION SELECT id ,username,password,role FROM users --"));
+console.log('Test 2:', searchProductSafe(''));
 
-console.log('Test 3:', loginSafe("admin' --", "anything"));
+console.log('Test 3:', searchProductSafe('<script>'));
 
-console.log('Test 4:', loginSafe("'OR '1' = '1","'OR '1' ='1"));
+// test case for log in
+console.log('Test 4:', loginSafe('admin', 'admin123'));
+
+console.log('Test 5:', loginSafe('admin' , 'ab'));
+
+console.log('Test 6:', loginSafe('ad min', 'pass123'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // testing
+// console.log('Test 1:', searchProductSafe("'OR 1=1--"));
+
+// console.log('Test 2:', searchProductSafe("'UNION SELECT id ,username,password,role FROM users --"));
+
+// console.log('Test 3:', loginSafe("admin' --", "anything"));
+
+// console.log('Test 4:', loginSafe("'OR '1' = '1","'OR '1' ='1"));
